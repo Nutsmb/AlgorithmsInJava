d1e2a0135c23b2fe6330735b9427b96334fd35c4
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2020-04-05 21:44:35 +0300

    Add sorting

--- src/ArraySort.java
+++ src/ArraySort.java
@@ -60,26 +60,87 @@ public class ArraySort {
         return i;
     }
 
+    public void bubbleSort(){
+        double tmp;
+        boolean isSorted = false;
+
+        while (!isSorted){
+            isSorted = true;
+            for (int i = 1; i < array.length; i++) {
+                if(array[i] < array[i-1]){
+                    tmp = array[i-1];
+                    array[i-1] = array[i];
+                    array[i] = tmp;
+                    isSorted = false;
+                }
+            }
+        }
+    }
+
+    public void selectionSort(){
+
+        for (int i = 0; i < array.length; i++) {
+            double min = array[i];
+            int minIndex = i;
+            for (int j = i+1; j < array.length; j++) {
+                if (array[j] < min) {
+                    min = array[j];
+                    minIndex = j;
+                }
+            }
+            if (i != minIndex) {
+                double tmp = array[i];
+                array[i] = array[minIndex];
+                array[minIndex] = tmp;
+            }
+        }
+    }
+
+    public void insertSort(){
+        for (int i = 1; i < array.length; i++) {
+            double current = array[i];
+            int j = i - 1;
+            while(j >= 0 && current < array[j]) {
+                array[j+1] = array[j];
+                j--;
+            }
+            array[j+1] = current;
+        }
+    }
+
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1000000;
         int pos = 3;
         double vol = 4.32;
         ArraySort mass = new ArraySort(n);
         mass.arrayRandom(15);
         mass.printArray();
         System.out.println();
+
         mass.add(vol);
         mass.printArray();
         System.out.println();
+
         mass.addArrayItem(pos,vol);
         mass.printArray();
         System.out.println();
+
         System.out.println(mass.searchArrayItem(vol));
+
         mass.deleteArrayItem(pos-1);
         mass.printArray();
         System.out.println();
+
         mass.deleteArrayItem(vol);
         mass.printArray();
         System.out.println();
+
+        long start = System.currentTimeMillis();
+        //mass.bubbleSort();
+        //mass.selectionSort();
+        mass.insertSort();
+        long finish = System.currentTimeMillis();
+        System.out.println();
+        System.out.println(finish - start);
     }
 }
