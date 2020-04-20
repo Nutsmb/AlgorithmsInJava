25f00a310222608d7081ce91974cf208a6798e45
Ayrat Kharrasov <airat.kharrasov@gmail.com>
2020-04-20 08:56:42 +0300

    Add Bagpack problem

---
+++ src/Lesson5/Item.java
@@ -0,0 +1,11 @@
+package Lesson5;
+
+public class Item {
+    int price;
+    int weight;
+
+    public Item(int price, int weight) {
+        this.price = price;
+        this.weight = weight;
+    }
+}
